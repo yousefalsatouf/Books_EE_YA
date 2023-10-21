9946caf0939ee0229dc37d2b35b737246ffe6c9f
yousefalsatouf <yousef@airobot.eu>
2023-10-21 16:48:37 +0200

    update the app and add logs and run it.

--- src/main/java/be/isl/books/BooksApplication.java
+++ src/main/java/be/isl/books/BooksApplication.java
@@ -1,13 +1,23 @@
 package be.isl.books;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BooksApplication {
+	private static final Logger logger = LoggerFactory.getLogger("info");
 
 	public static void main(String[] args) {
 		SpringApplication.run(BooksApplication.class, args);
+
+
+		try {
+			logger.info("APP is running on http://localhost:8080");
+		} catch (Exception e) {
+			logger.error("An error occurred: {}", e.getMessage(), e);
+		}
 	}
 
 }
